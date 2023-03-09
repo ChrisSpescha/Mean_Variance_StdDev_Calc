e2914505e55ea2306f678f8ac30819d1e332482d
Christopher Spescha <SpeschaChris@gmail.com>
2023-03-08 21:37:26 -0500

    more algorithm

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,31 +2,42 @@ import numpy as np
 import math
 
 def calculate(list):
-    # sort list for reasons
-    sorted_list = sorted(list)
-    
-    # can use numpy, but would like to exercise algorithms
-
-    # Mean
-    # (ps. research if faster way)
-    total = 0
-    for value in list:
-        total += value
-    mean = total / len(sorted_list)
-    
-    # Variance
-    variance_list = [(x - mean)**2 for x in sorted_list]
-    deviation_sum = sum(variance_list)
-    variance = deviation_sum / len(sorted_list)
-
-
-    # Standard Deviation
-    std_dev = math.sqrt(variance)
-
-
-    # NumPy solution
-    array = np.array(sorted_list)
-    # calculations = sorted_list
-    
-
-    return [mean, variance, std_dev]
+    if len(list) != 9:
+        return 'List must contain nine numbers.'
+    else:
+        results = {}
+        
+        array = np.array(list).reshape((3,3))
+        
+        rows_mean = []
+        rows_var = []
+        rows_std = []
+        rows_max = []
+        rows_min = []
+        rows_sum = []
+        for i in array:
+            rows_mean.append(i.mean())
+            rows_var.append(i.var())
+            rows_std.append(i.std())
+            rows_max.append(i.max())
+            rows_min.append(i.min())
+            rows_sum.append(i.sum())
+        print(rows_mean)
+        print(rows_var)
+        print(rows_std)
+        print(rows_max)
+        print(rows_min)
+        print(rows_sum)
+        
+        col_mean = []
+        col_var = []
+        col_std = []
+        col_max = []
+        col_min = []
+        col_sum = []
+        for i in array.T:
+            col_mean.append(i.mean())
+            col_var.append(i.var())
+            col_std.append(i.std())
+            
+        return array
